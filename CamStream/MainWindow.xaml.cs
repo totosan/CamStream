1e946a6e0220e61db25508af637df72d477c1208
toto_san@live.com <toto_san@live.com>
2015-02-14 11:57:15 +0100

    added some video sources

---
+++ CamStream/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CamStream
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeCam();
+        }
+
+        private void InitializeCam()
+        {
+            // enumerate video devices
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            // create video source
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+            // ...
+            // signal to stop when you no longer need capturing
+            videoSource.SignalToStop();
+            // ...
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // get new frame
+            Bitmap bitmap = eventArgs.Frame;
+            // process the frame
+
+        }
+
+    }
+
+
+
+}
